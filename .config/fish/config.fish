35fa19f928bee2bfc06624272af63417cf15a557
mitchpk <mitchellpkempen@gmail.com>
2021-03-30 12:05:02 +1030

    added dpi awareness to polybar

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 set -x DPI (cat ~/.Xresources | grep -o 'dpi.*' | cut -f2- -d':' | sed -e 's/^[[:space:]]*//')
 
+set -x BAR_HEIGHT (math "36 * ($DPI / 96)")
+
 if test $DPI -ge 144
 	set -x GDK_SCALE 2
 	set -x GDK_DPI_SCALE 0.5
