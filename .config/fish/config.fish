6cd328f963ffe90042e23b48fa07f7a2374f03e4
mitchpk <kphctim@gmail.com>
2021-09-26 23:47:53 +0930

    move to sway

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,3 @@
-set -x DPI (cat ~/.Xresources | grep -o 'dpi.*' | cut -f2- -d':' | sed -e 's/^[[:space:]]*//')
-set -x BAR_HEIGHT (math "32 * ($DPI / 96)")
-set -x TRAY_HEIGHT (math "$BAR_HEIGHT / 2")
 set -x TERMINAL "alacritty"
 set -x EDITOR "nvim"
 
@@ -28,11 +25,6 @@ if [ -f ~/quotes/quotes ]
 	end
 end
 
-if test $DPI -ge 144
-	set -x GDK_SCALE 2
-	set -x GDK_DPI_SCALE 0.5
-end
-
 if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]
-    exec startx
+    exec sway
 end
--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-path=$(pwd)
-
-ln -sf $path/.config/* ~/.config
-ln -sf $path/.xinitrc ~
-ln -sf $path/.vimrc ~
-cp $path/.Xresources ~
