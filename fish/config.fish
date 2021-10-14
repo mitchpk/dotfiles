0042f6c1a5023700cbe79e70c5d04935c91d0f85
mitchpk <mitchellpkempen@gmail.com>
2021-10-15 02:53:20 +1030

    get back X config and fix qt scaling

--- fish/config.fish
+++ fish/config.fish
@@ -32,9 +32,6 @@ end
 if test $DPI -ge 144
 	set -x GDK_SCALE 2
 	set -x GDK_DPI_SCALE 0.5
-	set -x QT_AUTO_SCREEN_SET_FACTOR 0
-	set -x QT_SCALE_FACTOR 2
-	set -x QT_FONT_DPI 96
 end
 
 if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]
