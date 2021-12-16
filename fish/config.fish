bfcca1df02bbd0b39c8331b962b8a6c11ac33af2
mitchpk <mitchellpkempen@gmail.com>
2021-12-17 00:05:02 +1030

    fix qt hidpi scaling

--- fish/config.fish
+++ fish/config.fish
@@ -13,28 +13,31 @@ alias v nvim
 alias g git
 
 if [ -f ~/quotes/quotes ]
-	function quote
-		if test (count $argv) -gt 0
-			set -l pwd (pwd)
-			cd ~/quotes
-			git pull > /dev/null
-			echo $argv >> quotes
-			git commit -am "new quote" > /dev/null
-			git push > /dev/null
-			cd $pwd
-		else
-			random choice (cat ~/quotes/quotes)
-		end
-	end
+    function quote
+        if test (count $argv) -gt 0
+            set -l pwd (pwd)
+            cd ~/quotes
+            git pull > /dev/null
+            echo $argv >> quotes
+            git commit -am "new quote" > /dev/null
+            git push > /dev/null
+            cd $pwd
+        else
+            random choice (cat ~/quotes/quotes)
+        end
+    end
 
-	function fish_greeting
-		#cowsay (random choice (cat ~/quotes/quotes))
-	end
+    function fish_greeting
+        #cowsay (random choice (cat ~/quotes/quotes))
+    end
 end
 
 if test $DPI -ge 144
-	set -x GDK_SCALE 2
-	set -x GDK_DPI_SCALE 0.5
+    set -x GDK_SCALE 2
+    set -x GDK_DPI_SCALE 0.5
+    set -x QT_AUTO_SCREEN_SET_FACTOR 0
+    set -x QT_SCALE_FACTOR 2
+    set -x QT_FONT_DPI 96
 end
 
 if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]
