2927d677aa4281be10c4dcbd7c0302a542348994
mitchpk <kphctim@gmail.com>
2022-04-18 18:05:18 +0930

    WSL is gone

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,14 @@
+set -x DPI (cat ~/.Xresources | grep -o 'dpi.*' | cut -f2- -d':' | sed -e 's/^[[:space:]]*//')
+set -x BAR_HEIGHT (math "32 * ($DPI / 96)")
+set -x TRAY_HEIGHT (math "$BAR_HEIGHT / 2")
+set -x TERMINAL "alacritty"
 set -x EDITOR "nvim"
-set -x COLORTERM truecolor
 set -x npm_config_prefix "$HOME/.local"
+set -x _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on'
 
 set -g theme_display_date no
+
+alias hx helix
 
 if [ -f ~/quotes/quotes ]
     function quote
@@ -22,4 +28,13 @@ if [ -f ~/quotes/quotes ]
     function fish_greeting
         #cowsay (random choice (cat ~/quotes/quotes))
     end
+end
+
+if test $DPI -ge 144
+	set -x GDK_SCALE 2
+	set -x GDK_DPI_SCALE 0.5
+end
+
+if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]
+	exec startx
 end
