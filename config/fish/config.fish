858ac4efeab1aab1c29ad63169c2f93fc70c792a
mitchpk <kphctim@gmail.com>
2022-07-12 15:30:43 +0930

    back to wsl?

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,7 @@
-set -x DPI (cat ~/.Xresources | grep -o 'dpi.*' | cut -f2- -d':' | sed -e 's/^[[:space:]]*//')
-set -x BAR_HEIGHT (math "32 * ($DPI / 96)")
-set -x TRAY_HEIGHT (math "$BAR_HEIGHT / 2")
 set -x TERMINAL "alacritty"
 set -x EDITOR "nvim"
 set -x npm_config_prefix "$HOME/.local"
-set -x _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on'
+set -x COLORTERM truecolor
 
 set -g theme_display_date no
 
@@ -35,13 +32,4 @@ if [ -f ~/quotes/quotes ]
     function fish_greeting
         #cowsay (random choice (cat ~/quotes/quotes))
     end
-end
-
-if test $DPI -ge 144
-	set -x GDK_SCALE 2
-	set -x GDK_DPI_SCALE 0.5
-end
-
-if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]
-	exec startx
 end
