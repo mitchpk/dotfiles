1847aab6c5b9a5ab20761cf775b0b2d34a24bd2d
mitchpk <kphctim@gmail.com>
2023-01-01 15:00:22 +1030

    stop random jumps

--- config/nvim/plugin/cmp.rc.lua
+++ config/nvim/plugin/cmp.rc.lua
@@ -57,3 +57,18 @@ cmp.setup({
         { name = 'path' }
     })
 })
+
+function leave_snippet()
+    if
+        ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+        and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require('luasnip').session.jump_active
+    then
+        require('luasnip').unlink_current()
+    end
+end
+
+-- stop snippets when you leave to normal mode
+vim.api.nvim_command([[
+    autocmd ModeChanged * lua leave_snippet()
+]])
