1d009e1135fd1caabdd0729ba03b75486ca766ed
mitchpk <kphctim@gmail.com>
2023-01-01 21:50:36 +1030

    dots

--- config/nvim/plugin/lspconfig.lua
+++ config/nvim/plugin/lspconfig.lua
@@ -81,14 +81,14 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     {
         underline = true,
         update_in_insert = false,
-        virtual_text = { spacing = 4, prefix = "● " },
+        virtual_text = { spacing = 4, prefix = "●" },
         severity_sort = true,
     }
 )
 
 vim.diagnostic.config({
     virtual_text = {
-        prefix = '● '
+        prefix = '●'
     },
     update_in_insert = true,
     float = {
