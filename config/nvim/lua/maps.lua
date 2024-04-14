6c596d83ddc707eec544f89dda2fb40814c51029
mitchpk <kphctim@gmail.com>
2024-04-14 12:33:13 +0930

    no zz

--- config/nvim/lua/maps.lua
+++ config/nvim/lua/maps.lua
@@ -1,38 +1,31 @@
-local keymap = vim.keymap
-
 vim.g.mapleader = ','
-keymap.set('n', ',,', ',')
+vim.keymap.set('n', ',,', ',')
 
-keymap.set('n', '<C-d>', '<C-d>zz')
-keymap.set('n', '<C-u>', '<C-u>zz')
-keymap.set('n', 'n', 'nzz')
-keymap.set('n', 'N', 'Nzz')
+vim.keymap.set('n', 'x', '"_x')
+vim.keymap.set('x', '<leader>p', '"_dP')
+vim.keymap.set('n', '<leader>Y', '"+Y')
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y')
+vim.keymap.set({'n', 'v'}, '<leader>d', '"_d')
 
-keymap.set('n', 'x', '"_x')
-keymap.set('x', '<leader>p', '"_dP')
-keymap.set('n', '<leader>Y', '"+Y')
-keymap.set({'n', 'v'}, '<leader>y', '"+y')
-keymap.set({'n', 'v'}, '<leader>d', '"_d')
+vim.keymap.set('n', '<C-n>', '<cmd>cnext<CR>')
+vim.keymap.set('n', '<C-p>', '<cmd>cprev<CR>')
 
-keymap.set('n', '<C-n>', '<cmd>cnext<CR>zz')
-keymap.set('n', '<C-p>', '<cmd>cprev<CR>zz')
+vim.keymap.set('n', '+', '<C-a>')
+vim.keymap.set('n', '-', '<C-x>')
 
-keymap.set('n', '+', '<C-a>')
-keymap.set('n', '-', '<C-x>')
+vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
 
-keymap.set('n', '<C-a>', 'gg<S-v>G')
+vim.keymap.set('n', 'ss', ':split<Return><C-w>w')
+vim.keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
 
-keymap.set('n', 'ss', ':split<Return><C-w>w')
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+vim.keymap.set('n', '<Space>', '<C-w>w')
+vim.keymap.set('', 'sh', '<C-w>h')
+vim.keymap.set('', 'sk', '<C-w>k')
+vim.keymap.set('', 'sj', '<C-w>j')
+vim.keymap.set('', 'sl', '<C-w>l')
+vim.keymap.set('', 'sq', '<C-w>q')
 
-keymap.set('n', '<Space>', '<C-w>w')
-keymap.set('', 'sh', '<C-w>h')
-keymap.set('', 'sk', '<C-w>k')
-keymap.set('', 'sj', '<C-w>j')
-keymap.set('', 'sl', '<C-w>l')
-keymap.set('', 'sq', '<C-w>q')
-
-keymap.set('n', '<C-w><left>', '<C-w><')
-keymap.set('n', '<C-w><right>', '<C-w>>')
-keymap.set('n', '<C-w><up>', '<C-w>+')
-keymap.set('n', '<C-w><down>', '<C-w>-')
+vim.keymap.set('n', '<C-w><left>', '<C-w><')
+vim.keymap.set('n', '<C-w><right>', '<C-w>>')
+vim.keymap.set('n', '<C-w><up>', '<C-w>+')
+vim.keymap.set('n', '<C-w><down>', '<C-w>-')
